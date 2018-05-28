777f484b4b71e94340f5f28e05c0eb21293472f7
saiteja93 <saiteja.sirikonda@gmail.com>
2018-05-27 18:53:06 -0700

    done with the read_data

---
+++ machine_trans.py
@@ -0,0 +1,7 @@
+#Author: Saiteja Sirikonda
+#Project : A simple Machine Translation system using Keras
+#Start date: May 22nd, 2018
+
+#Dataset taken from page www.manythings.org/anki/
+#Trying to train on a Dataset which has the highest number of samples available in the page
+#German - English, in the page's current version.
